05599da1a91a64725fb7c3e00bd694ab3b4c538d
DESKTOP-UBDJMTN\Hulkxp <omar.ehab60@gmail.com>
2017-02-12 22:17:44 +0200

    Initial Commit

---
+++ BuildingE/Source/BuildingE/BuildingEGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingE.h"
+#include "BuildingEGameMode.h"
+
+
+
+
---
+++ BuildingE/Source/BuildingE/BuildingEGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BuildingEGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGE_API ABuildingEGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
